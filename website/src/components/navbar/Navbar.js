98c9f12324b21ccb0940de9c7d3321ce7131c5cd
rankboost <vikasisgen@gmail.com>
2022-11-28 13:34:29 +0530

    fck

--- website/src/components/navbar/Navbar.js
+++ website/src/components/navbar/Navbar.js
@@ -88,13 +88,13 @@ const Navbar = () => {
         <>
 
 
-<div className='mobile-offer-top activate-dis'>
-     <ul>
-        <li>Made by IITIANs and Top Coaches</li>
-        <li>A Quality product by RankBoost</li>
-        <li>India's Best Mentorship platform</li>
-     </ul>
-     </div>
+            <div className='mobile-offer-top activate-dis'>
+                <ul>
+                    <li>Made by IITIANs and Top Coaches</li>
+                    <li>A Quality product by RankBoost</li>
+                    <li>India's Best Mentorship platform</li>
+                </ul>
+            </div>
             <div id="navbar-main"
                 className="navbar-main" >
                 <LoadingBar
@@ -128,7 +128,7 @@ const Navbar = () => {
 
 
                         <li><a style={location.pathname === '/refral' ? { color: 'var(--s2)', } : { color: 'var(--c1)', borderColor: 'var(--c1)' }} onClick={() => { navigate('/refral') }}>Referral</a></li>
-                        <li><a style={location.pathname === '/books' ? { color: 'var(--s2)', } : { color: 'var(--c1)', borderColor: 'var(--c1)' }} onClick={() => { navigate('/books') }}>Books</a></li>
+                        {/* <li><a style={location.pathname === '/books' ? { color: 'var(--s2)', } : { color: 'var(--c1)', borderColor: 'var(--c1)' }} onClick={() => { navigate('/books') }}>Books</a></li> */}
                         <li><a style={location.pathname === '/guidance-personal' ? { color: 'var(--s2)', } : { color: 'var(--c1)', borderColor: 'var(--c1)' }} onClick={() => { navigate('/guidance-personal') }}>Personal 1-1 guidance</a></li>
                         <li><a style={location.pathname === '/support' ? { color: 'var(--s2)', } : { color: 'var(--c1)', borderColor: 'var(--c1)' }} onClick={() => { navigate('/support') }}>Customer support</a></li>
                         <li><a onClick={() => { navigate("/checkout") }}><p>{cartlength}</p><img src={cart}></img></a></li>
@@ -185,7 +185,7 @@ const Navbar = () => {
                         <li onClick={() => { navigate('/courses'); show(); }}> <div className="menu-bros" ><img className="menu-icos" src={mcourse} ></img> <a>Courses</a></div><img src={mright}></img></li>
                         <li onClick={() => { navigate('/price'); show(); }}> <div className="menu-bros" ><img className="menu-icos" src={mprice} ></img> <a>Pricing</a></div><img src={mright}></img></li>
                         <li onClick={() => { navigate('/refral'); show(); }}> <div className="menu-bros" ><img className="menu-icos" src={mrefer} ></img> <a >Referral</a></div><img src={mright}></img></li>
-                        <li onClick={() => { navigate('/books'); show(); }}> <div className="menu-bros" ><img className="menu-icos" src={mbook} ></img> <a >Books</a></div><img src={mright}></img></li>
+                        {/* <li onClick={() => { navigate('/books'); show(); }}> <div className="menu-bros" ><img className="menu-icos" src={mbook} ></img> <a >Books</a></div><img src={mright}></img></li> */}
                         <li onClick={() => { navigate('/guidance-personal'); show(); }}> <div className="menu-bros" ><img className="menu-icos" src={mdoubt} ></img> <a>Personal 1-1 guidance</a></div><img src={mright}></img></li>
                         <li onClick={() => { navigate('/forgot'); show(); }}> <div className="menu-bros" ><img className="menu-icos" src={mkey} ></img> <a >Change &nbsp; password</a></div><img src={mright}></img></li>
                         <li onClick={() => { navigate('/support'); show(); }}> <div className="menu-bros" ><img className="menu-icos" src={msupport} ></img> <a >Customer &nbsp; support</a></div><img src={mright}></img></li>
@@ -204,10 +204,11 @@ const Navbar = () => {
                         <img src={mrefer}></img>
                         <h1>Refer to Earn</h1>
                     </div>
-                    <div onClick={() => { navigate('/books') }} className="bottom-menu-comman">
+                    {/* <div onClick={() => { navigate('/books') }} className="bottom-menu-comman">
                         <img src={mbook}></img>
                         <h1>Books</h1>
-                    </div>
+                    </div> */}
+                    
                     <div onClick={() => { show() }} className="bottom-menu-center">
                         <img src={dottedmenu}></img>
                     </div>
